fa969094781533fed2573bf0da0efdc339a782f8
Victor França <victorfclima@gmail.com>
2020-06-25 15:53:28 -0300

    New: Add getBalance method

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -10,8 +10,11 @@ const transactionsRouter = Router();
 
 transactionsRouter.get('/', async (request, response) => {
   const transactionsRepository = getCustomRepository(TransactionsRepository);
+
   const transactions = await transactionsRepository.find();
-  return response.json(transactions);
+  const balance = await transactionsRepository.getBalance();
+
+  return response.json({ transactions, balance });
 });
 
 transactionsRouter.post('/', async (request, response) => {
